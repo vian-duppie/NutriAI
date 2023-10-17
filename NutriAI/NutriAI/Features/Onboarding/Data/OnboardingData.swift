5db519c0d7ec28accdee0f99ba7aec61f44ca141
Vian du Plessis <125647194+vian-duppie@users.noreply.github.com>
2023-10-17 21:21:08 +0200

    Data - Onboarding Data Added

---
+++ NutriAI/NutriAI/Features/Onboarding/Data/OnboardingData.swift
@@ -0,0 +1,13 @@
+//
+//  OnboardingData.swift
+//  NutriAI
+//
+//  Created by Vian du Plessis on 2023/10/17.
+//
+
+import Foundation
+
+let onboardingData: [Onboarding] = [
+   Onboarding(rectangleColor: "CustomOrange", rectangeCircleColor: "CustomLightBlue", leafColor: "CustomYellow", leafCircleColor: "CustomDarkBlue", image: "PastaImage", imageBackgroundColor: "CustomLightBlue", title: "Instant", title2: "Nutritional \nAnalysis"),
+   Onboarding(rectangleColor: "CustomLightBlue", rectangeCircleColor: "CustomOrange", leafColor: "CustomLightBlue", leafCircleColor: "CustomYellow", image: "SoupImage", imageBackgroundColor: "CustomDarkBlue", title: "Custom", title2: "Meal \nSuggestions")
+]
