598fd37465eaf50371caa205ef79231e426de262
Vian du Plessis <125647194+vian-duppie@users.noreply.github.com>
2023-10-17 21:21:22 +0200

    Model - Onboarding Model Created

---
+++ NutriAI/NutriAI/Features/Onboarding/Model/OnboardingModel.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingModel.swift
+//  NutriAI
+//
+//  Created by Vian du Plessis on 2023/10/17.
+//
+
+import Foundation
+
+struct Onboarding: Identifiable {
+    var id = UUID()
+    var rectangleColor: String
+    var rectangeCircleColor: String
+    var leafColor: String
+    var leafCircleColor: String
+    var image: String
+    var imageBackgroundColor: String
+    var title: String
+    var title2: String    
+}
