ce98bab9f227efb4d4f10903356a2e12acf1c53a
Vian du Plessis <125647194+vian-duppie@users.noreply.github.com>
2023-11-08 22:39:19 +0200

    Update SplashView.swift

--- NutriAI/NutriAI/Features/SplashView/Views/SplashView.swift
+++ NutriAI/NutriAI/Features/SplashView/Views/SplashView.swift
@@ -31,11 +31,9 @@ struct SplashView: View {
                             .foregroundColor(Color("CustomLightGreen"))
                     }
                   
-                    
                     Text("your personal nutrition assistant")
                         .font(.custom("Poppins-Regular", size: 18))
                         .foregroundColor(Color("CustomDarkGreen"))
-//                        .fontWeight(.bold)
                 }
                 .padding(.horizontal, 35)
                 .frame(maxWidth: .infinity, alignment: .leading)
@@ -51,82 +49,22 @@ struct SplashView: View {
                 .frame(maxWidth: .infinity, alignment: .bottomTrailing)
                 
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+//            .background(.black)
             .ignoresSafeArea(.all, edges: .bottom)
             .onAppear {
-                withAnimation(.interpolatingSpring(stiffness: 100, damping: 7, initialVelocity: 0.1)) {
-                    animateSpring = true
-                }
-                
-                withAnimation(.easeInOut(duration: 1)) {
-                    animate = true
-                }
-            }
-
-    }
-}
-
-struct RectangleSemiCircleView: View {
-    @State var screenSize = UIScreen.main.bounds
-    @Binding var animateSpring: Bool
-    var rectangleColor: String
-    var circleColor: String
-    
-    var body: some View {
-        VStack {
-            ZStack(alignment: .bottomLeading) {
-                VStack(spacing: 30) {
-                    ForEach(0..<4) {_ in
-                        Rectangle()
-                            .frame(width:  screenSize.size.width * 0.3, height: 13)
-                            .foregroundColor(Color(rectangleColor))
+                DispatchQueue.main.async {
+                    withAnimation(.interpolatingSpring(stiffness: 100, damping: 7, initialVelocity: 0.1)) {
+                        animateSpring = true
+                    }
+                    
+                    withAnimation(.easeInOut(duration: 1)) {
+                        animate = true
                     }
                 }
-                
-                Circle()
-                    .frame(width: animateSpring ? 80 : 0, alignment: .leading)
-                    .foregroundColor(Color(circleColor))
-                    .offset(x: -(80*0.7), y: 35)
-                    .zIndex(-1)
             }
-        }
-        .frame(maxWidth: .infinity, alignment: .leading)
     }
 }
-
-struct LeafShapeAndImageView: View {
-    @Binding var animate: Bool
-    var circleColor: String
-    var leafColor: String
-    var imageBackgroundColor: String
-    var image: String
-    
-    var body: some View {
-        HStack {
-            VStack {
-                Circle()
-                    .frame(width: animate ? 50 : 0)
-                    .foregroundColor(Color(circleColor))
-                
-                LeafShape()
-                    .frame(width: animate ? 70 : 0, height: 70)
-                    .foregroundColor(Color(leafColor))
-            }
-            
-            Image(image)
-                .resizable()
-                .frame(width: 190, height: 190)
-                .padding(10)
-                .background(Color(imageBackgroundColor))
-                .clipShape(Circle())
-              
-        }
-        .frame(maxWidth: .infinity, alignment: .trailing)
-        .offset(x: 60)
-    }
-}
-
-
-
 
 #Preview {
     SplashView()
